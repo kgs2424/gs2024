2b1cbc937ca9411907937d14c2e92b1b5480b352
kgs2424 <choop7866@naver.com>
2024-11-15 13:21:28 +0900

    test

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,3 +1,4 @@
+import { ArticleCard } from '@/components/test/test';
 import { ColorSchemeToggle } from '../components/ColorSchemeToggle/ColorSchemeToggle';
 import { Welcome } from '../components/Welcome/Welcome';
 
@@ -6,6 +7,7 @@ export function HomePage() {
     <>
       <Welcome />
       <ColorSchemeToggle />
+      <ArticleCard />
     </>
   );
 }
